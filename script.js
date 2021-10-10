129770b7b75081da867a6b644628f9384a2e48b4
thaynamarinss <thayna.mgas@hotmail.com>
2021-10-10 11:50:37 -0300

    Desafio Fcamara

---
+++ script.js
@@ -0,0 +1,5 @@
+swal({
+    title: "Atenção!",
+    text: "A ida aos escritórios da Fcamara é totalmente opcional, ficando a critério do consultor permanecer, ou não, em casa.",
+    icon: "warning",
+});
